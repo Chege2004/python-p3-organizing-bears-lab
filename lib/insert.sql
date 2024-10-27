61213df94b49c2741d31086adc23ac1542629389
Manasseh <chegemanasseh2@gmail.com>
2024-10-27 12:35:21 +0300

    Added the code for the creation ,inserting and retrieval of information

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+--Creation of table values
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr.Chocolate', 10, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 5, 'M', 'Black', 'Aggressive', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 4, 'F', 'Brown', 'Friendly', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant', 7, 'M', 'Gray', 'Stoic', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 3, 'F', 'Black', 'Playful', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 12, 'F', 'White', 'Grumpy', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 9, 'F', 'Brown', 'Gentle', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 2, 'M', 'Brown', 'Unknown', 1);
